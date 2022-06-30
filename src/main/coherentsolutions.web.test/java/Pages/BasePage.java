01469c5617b651131fa331ebd44fcaaa5c72a13f
cath isina <isinacath378@gmail.com>
2022-06-30 23:00:20 +0400

    - Adding SnapShot method to Base Page - Adding method to Steps to YandexTest

--- src/main/coherentsolutions.web.test/java/Pages/BasePage.java
+++ src/main/coherentsolutions.web.test/java/Pages/BasePage.java
@@ -1,12 +1,12 @@
 package Pages;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
-import static Managers.WebDriverManager.initDriver;
+import java.io.File;
 
 public class BasePage {
 
@@ -17,5 +17,12 @@ public class BasePage {
         this.driver = driver;
         this.webDriverWait = webDriverWait;
         PageFactory.initElements(driver, this);
+    }
+
+    public static void takeSnapShot(WebDriver webdriver, String fileWithPath) throws Exception {
+        TakesScreenshot scrShot = ((TakesScreenshot) webdriver);
+        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+        File DestFile = new File(fileWithPath);
+        org.apache.commons.io.FileUtils.copyFile(SrcFile, DestFile);
     }
 }
--- src/test/coherentsolutions.web.test/java/TestCases/YandexTest.java
+++ src/test/coherentsolutions.web.test/java/TestCases/YandexTest.java
@@ -11,15 +11,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
-
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.time.Duration;
 
 import static Managers.WebDriverManager.initDriver;
 import static Managers.WebDriverManager.quitDriver;
+import static Pages.BasePage.takeSnapShot;
 import static Utils.ConstantUtils.YANDEX_LOGIN_PAGE_URL;
-import static Utils.PauseUtils.pause;
+import static java.time.LocalTime.now;
 
 public class YandexTest {
 
@@ -36,29 +35,34 @@ public class YandexTest {
     }
 
     @Test(description = "Checking login and log out with valid credentials")
-    public void loginTest() throws IOException, InterruptedException {
+    public void loginTest() throws Exception {
         initDriver().get(YANDEX_LOGIN_PAGE_URL);
         StartPage startPage = new StartPage(driver, webDriverWait);
         Assert.assertTrue(startPage.isPageLoaded(), "Start Page is not displayed");
+        takeSnapShot(driver, String.format("screenshots/Step1_%s.png", now()));
         log.info("Start Page opened");
 
         LoginPage loginPage = startPage.clickLoginButton();
         Assert.assertTrue(loginPage.isLoginFieldDisplayed(), "Login Page is not displayed");
+        takeSnapShot(driver, String.format("screenshots/Step2_%s.png", now()));
         log.info("Login Page opened");
 
         loginPage.enterLogin(propertiesUtils.getProperty("user1"));
         loginPage.clickLogin();
         Assert.assertTrue(loginPage.isPasswordFieldDisplayed(), "Password Field is not displayed");
+        takeSnapShot(driver, String.format("screenshots/Step3_%s.png", now()));
         log.info("Password Field appeared");
 
         loginPage.enterPassword(propertiesUtils.getProperty("password1"));
         MailPage mailPage = loginPage.clickLogin();
         Assert.assertTrue(mailPage.isUsernameDisplay(), "Messages Page is not displayed, Login failed.");
+        takeSnapShot(driver, String.format("screenshots/Step4_%s.png", now()));
         log.info("User logged in successfully");
 
         mailPage.clickSettings();
         mailPage.logOut();
         Assert.assertTrue(loginPage.isPasswordFieldDisplayed(), "Login Page is opened, user is logged out.");
+        takeSnapShot(driver, String.format("screenshots/Step1_%5.png", now()));
         log.info("User logged out successfully");
     }
 
